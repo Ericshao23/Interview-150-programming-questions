e631c6cedc3278e69c44222b0c4b426bd5f8a410
Ericshao23 <cnpshao@gmail.com>
2023-07-21 16:07:49 +0800

    heap sort

--- heap_sort.py
+++ heap_sort.py
@@ -1,41 +1,36 @@
-from collections import deque
+class Solution(object):
 
-def swamp(lists, i, j):
-    lists[i], lists[j] = lists[j], lists[i]
-    return lists
+    def build_heap(self, i, l):
+        '''
+        构建大顶堆
+        即每个节点的值都大于等于其叶子的值
+        '''
+        arr = self.arr
+        left, right = 2 * i + 1, 2 * i + 2 # 左右子节点下标
+        large_index = i
+        if left <= l and arr[i] < arr[left]:
+            large_index = left
+        if right <= l and arr[left] < arr[right]:
+            large_index = right
+        if large_index != i: # 如果三元素最大值不是父节点下标，则交换后需要调整大顶堆
+            arr[i], arr[large_index] = arr[large_index], arr[i]
+            self.build_heap(large_index, l)
 
-def heap_adjust(lists, start, end):
-    temp = lists[start]
-
-    i = start
-    j = 2 * i
-
-    while j <= end:
-        if (j < end) and (lists[j] < lists[j + 1]):
-            j += 1
-        if temp < lists[j]:
-            lists[i] = lists[j]
-            i = j
-            j = 2 * i
-        else:
-            break
-    lists[i] = temp
-
-def heap_sort(lists):
-    length = len(lists) - 1
-    first_sort_count = length // 2
-    for i in range(first_sort_count):
-        heap_adjust(lists, first_sort_count - i, length)
-
-    for i in range(length - 1):
-        lists = swamp(lists, 1, length -i)
-        heap_adjust(lists, 1, length - i -1)
-    return [lists[i] for i in range(1, len(lists))]
+    def heap_sort(self, arr):
+        """
+        堆排序，基于完全二叉树结构的一种排序
+        通过构建大顶堆（即每个节点的值都大于等于其叶子的值），完成升序排列
+        """
+        i, l = 0, len(arr)
+        self.arr = arr
+        for i in range(l // 2 - 1, -1, -1): # 构建大顶堆，从最后一个非叶子节点开始
+            self.build_heap(i, l - 1)
+        for j in range(l - 1, -1, -1): # 根节点和最后一个元素交换，然后调整大顶堆
+            arr[0], arr[j] = arr[j], arr[0]
+            self.build_heap(0, j - 1)
+        return arr
 
 
-
-if __name__ == '__main__':
-    lists = deque([50, 16, 30, 10, 60,  90,  2, 80, 70])
-    # lists = [50, 16, 30, 10, 60,  90,  2, 80, 70]
-    lists.appendleft(0)
-    print(heap_sort(lists))
+test_list = [4, 2, 1, 23, 5, 12, 13, 14, 15, 9, 2, 3]
+test_sort = Solution()
+print(test_sort.heap_sort(test_list))
