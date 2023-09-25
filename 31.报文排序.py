bc67effcb4dad4e03d56568f26093850095a30a5
Ericshao23 <cnpshao@gmail.com>
2023-09-25 17:12:49 +0800

    sort

--- 31.报文排序.py
+++ 31.报文排序.py
@@ -1,32 +1,28 @@
-# [31] 报文排序
-# 输入获取
+#
+# @lc app=leetcode.cn id=1859 lang=python3
+#
+# [1859] 将句子排序
+#
+
+# @lc code=start
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        ans = []
+        reg = re.compile(r"([a-zA-Z]+)(\d+)")
+        arr = s.split()
+    
+        for s in arr:
+            matcher = reg.search(s)
+    
+            if matcher is None:
+                continue
+    
+            content = matcher.group(1)
+            i = int(matcher.group(2)) - 1
+    
+            ans.append([i, content])
+    
+        ans.sort(key=lambda x: x[0])
+    
+        return " ".join(map(lambda x: x[1], ans))
 import re
- 
-n = int(input())
-arr = input().split()
- 
- 
-# 算法入口
-def getResult():
-    ans = []
- 
-    reg = re.compile(r"([a-zA-Z]+)(\d+)")
- 
-    for s in arr:
-        matcher = reg.search(s)
- 
-        if matcher is None:
-            continue
- 
-        content = matcher.group(1)
-        i = int(matcher.group(2)) - 1
- 
-        ans.append([i, content])
- 
-    ans.sort(key=lambda x: x[0])
- 
-    return " ".join(map(lambda x: x[1], ans))
- 
- 
-# 算法调用
-print(getResult())
